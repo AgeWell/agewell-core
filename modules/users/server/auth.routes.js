c585acfe001bbccc6f8a4292c73b324c874c9aba
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-02-16 16:45:11 -0600

    loadied in the package.json and user module.

---
+++ modules/users/server/auth.routes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var passport = require('passport');
+
+module.exports = function (app) {
+  // User Routes
+  var users = require('./users.controller');
+
+  // Setting up the users authentication api
+  app.route('/api/auth/signout').get(users.signout);
+
+  // Setting the google oauth routes
+  app.route('/api/auth/google').get(users.oauthCall('google', {
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  }));
+  app.route('/api/auth/google/callback').get(users.oauthCallback('google'));
+
+  app.route('/api/auth/harvest').get(users.oauthCall('harvest'));
+  app.route('/api/auth/harvest/callback').get(users.harvestCallback('harvest'));
+};
