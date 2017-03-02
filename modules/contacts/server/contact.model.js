5977d499b32eac853963d785748b15e1ad9ce550
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-03-02 10:58:01 -0600

    Began modeling the contacts

--- modules/contacts/server/contact.model.js
+++ modules/contacts/server/contact.model.js
@@ -3,22 +3,58 @@
 /**
  * Module dependencies.
  */
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+let mongoose = require('mongoose'),
+  Schema = mongoose.Schema,
+  validator = require('validator');
+
+/**
+ * A Validation function for local strategy email
+ */
+let validateLocalStrategyEmail = function(email) {
+  return (validator.isEmail(email, {
+    require_tld: false
+  }));
+};
+
 
 /**
  * Contact Schema
  */
-var ContactSchema = new Schema({
-  name: {
+let ContactSchema = new Schema({
+  firstName: {
     type: String,
-    default: '',
-    required: 'Please fill Contact name',
+    required: 'Please fill first name',
     trim: true
+  },
+  middleInitial: {
+    type: String,
+    maxlength: 1
+  },
+  lastName: {
+    type: String,
+    required: 'Please fill last name',
+    trim: true
+  },
+  phones: [{
+    number: String,
+    type: {
+      type: String,
+      enum: ['Home', 'Work', 'Cell']
+    }
+  }],
+  email: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    default: '',
+    validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
   },
   created: {
     type: Date,
     default: Date.now
+  },
+  updated: {
+    type: Date
   },
   user: {
     type: Schema.ObjectId,
