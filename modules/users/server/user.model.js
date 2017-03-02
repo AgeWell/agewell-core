dc7b25463a1a24d8c7446c9a30d12bceb0341366
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-03-02 17:40:23 -0600

    corrected the default image path.

--- modules/users/server/user.model.js
+++ modules/users/server/user.model.js
@@ -99,7 +99,7 @@ var UserSchema = new Schema({
   },
   profileImageURL: {
     type: String,
-    default: 'modules/users/client/img/profile/default.png'
+    default: '/modules/users/client/assets/images/profile/default.png'
   },
   provider: {
     type: String,
