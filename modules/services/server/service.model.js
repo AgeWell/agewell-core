cac445425d992d1b0f7159fceae47964dec06a08
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-03-10 13:34:39 -0600

    updated the service model.

--- modules/services/server/service.model.js
+++ modules/services/server/service.model.js
@@ -10,16 +10,51 @@ const Schema = mongoose.Schema;
  * Service Schema
  */
 let ServiceSchema = new Schema({
-  name: {
+  title: {
     type: String,
     default: '',
-    required: 'Please fill Service name',
+    required: 'Please enter a title',
     trim: true
   },
+  description: {
+    type: String,
+    default: '',
+    required: 'Please enter a description',
+    trim: true
+  },
+  avalibility: {
+    type: String,
+    enum: ['On-demand', 'Weekly', 'Monthly'],
+    required: 'Please choose an avalibility'
+  },
+  price: {
+    type: Number,
+    required: 'Please set a price'
+  },
+  pricePer: {
+    type: String,
+    enum: ['Hour', 'Unit', 'Month'],
+    required: 'Please choose pricing unit'
+  },
+  unitName: String,
+  // neededSkill: {
+  //   type: [String],
+  //   enum: []
+  // },
   created: {
     type: Date,
     default: Date.now
   }
+});
+
+// Getter
+ServiceSchema.path('price').get(function(num) {
+  return (num / 100).toFixed(2);
+});
+
+// Setter
+ServiceSchema.path('price').set(function(num) {
+  return num * 100;
 });
 
 mongoose.model('Service', ServiceSchema);
