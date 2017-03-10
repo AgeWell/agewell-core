12bef87bf54b3f2e21fe3265990426f5cfd56a94
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-03-10 12:03:59 -0600

    Cleaned up user module and seperated service into its own file.

---
+++ modules/users/client/admin/users-admin.service.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  // TODO this should be Users service
+  angular
+    .module('users.admin.services')
+    .factory('AdminService', AdminService);
+
+  AdminService.$inject = ['$resource'];
+
+  function AdminService($resource) {
+    return $resource('/api/users/:userId', {
+      userId: '@_id'
+    }, {
+      update: {
+        method: 'PUT'
+      }
+    });
+  }
+}());
