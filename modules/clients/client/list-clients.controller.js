294c4effdce3516fa0b85c25bd12620d85610420
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-03-17 12:41:28 -0500

    Added some addtional features to the client and vol pages.

--- modules/clients/client/list-clients.controller.js
+++ modules/clients/client/list-clients.controller.js
@@ -5,12 +5,23 @@
     .module('clients')
     .controller('ClientsListController', ClientsListController);
 
-  ClientsListController.$inject = ['ClientsService'];
+  ClientsListController.$inject = ['$filter', 'ClientsService'];
 
-  function ClientsListController(ClientsService) {
+  function ClientsListController($filter, ClientsService) {
     var vm = this;
+    vm.figureOutItemsToDisplay = figureOutItemsToDisplay;
 
     vm.clients = ClientsService.query();
+
+    function figureOutItemsToDisplay() {
+      vm.filteredItems = $filter('filter')(vm.services, {
+        $: vm.search
+      });
+      vm.filterLength = vm.filteredItems.length;
+      var begin = ((vm.currentPage - 1) * vm.itemsPerPage);
+      var end = begin + vm.itemsPerPage;
+      vm.pagedItems = vm.filteredItems.slice(begin, end);
+    }
 
     console.log(vm);
   }
