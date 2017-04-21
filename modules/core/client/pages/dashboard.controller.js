d775c18e5bbbad0a60e9969aeb46ba89d42af2d4
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-04-20 22:38:12 -0500

    working on the templates a little.

--- modules/core/client/pages/dashboard.controller.js
+++ modules/core/client/pages/dashboard.controller.js
@@ -7,5 +7,16 @@
 
   function DashboardController() {
     var vm = this;
+    vm.orders = [];
+
+    for (var i = 0; i < 5; i++) {
+      vm.orders.push({
+        clientId: i,
+        orderId: i,
+        requestNumber: '# ' + i,
+        name: 'Jane Doe',
+        status: 'Active'
+      });
+    }
   }
 }());
