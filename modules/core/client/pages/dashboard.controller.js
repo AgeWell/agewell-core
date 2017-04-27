d17fbabf3fee51889ae95dce35b5dcab803ba0a8
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-04-27 09:53:41 -0500

    Adjusting the dash template

--- modules/core/client/pages/dashboard.controller.js
+++ modules/core/client/pages/dashboard.controller.js
@@ -5,15 +5,38 @@
     .module('core')
     .controller('DashboardController', DashboardController);
 
-  function DashboardController() {
+  DashboardController.$inject = ['$scope', '$state', '$window', 'Authentication', 'Notification', 'coreService'];
+
+  function DashboardController($scope, $state, $window, Authentication, Notification, coreService, client) {
     var vm = this;
+    vm.options = coreService.getOptions('Order');
+    vm.callList = [];
     vm.orders = [];
 
+    console.log(vm);
+
+    vm.dates = {
+      now: new Date(),
+      orderBy: new Date(vm.options.order[0]),
+      nextOrderBy: new Date(vm.options.order[1]),
+      delivery: new Date(vm.options.delivery[0]),
+      nextDelivery: new Date(vm.options.delivery[1])
+    };
+
     for (var i = 0; i < 5; i++) {
+      vm.callList.push({
+        _id: i,
+        orderID: i,
+        name: 'Jane Doe',
+        homePhone: '555-555-5555'
+      });
+    }
+
+    for (var j = 0; j < 5; j++) {
       vm.orders.push({
-        clientId: i,
-        orderId: i,
-        requestNumber: '# ' + i,
+        clientId: j,
+        orderId: j,
+        requestNumber: '# ' + j,
         name: 'Jane Doe',
         status: 'Active'
       });
