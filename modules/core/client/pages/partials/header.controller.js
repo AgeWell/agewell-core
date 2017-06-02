5a6bdcd7d0b8b864542585475851025ddccbc855
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-06-01 21:48:49 -0500

    remaining cleanup on the core module.

--- modules/core/client/pages/partials/header.controller.js
+++ modules/core/client/pages/partials/header.controller.js
@@ -12,7 +12,6 @@
 
     vm.accountMenu = menuService.getMenu('account').items[0];
     vm.authentication = Authentication;
-    // vm.callOauthProvider = callOauthProvider;
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
 
@@ -22,15 +21,5 @@
       // Collapsing the menu after navigation
       vm.isCollapsed = false;
     }
-
-    // // OAuth provider request
-    // function callOauthProvider(url) {
-    //   if ($state.previous && $state.previous.href) {
-    //     url += '?redirect_to=' + encodeURIComponent($state.previous.href);
-    //   }
-    //
-    //   // Effectively call OAuth authentication route:
-    //   $window.location.href = url;
-    // }
   }
 }());
