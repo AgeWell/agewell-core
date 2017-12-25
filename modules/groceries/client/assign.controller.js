fdfcedb4d8b8c873098202eb87f85e3630f5cc6f
Tristan Fitzgerald <tristan@simplyspoke.com>
2017-12-24 18:53:31 -0600

    Updated the assignment screen Fixes #18

--- modules/groceries/client/assign.controller.js
+++ modules/groceries/client/assign.controller.js
@@ -5,9 +5,9 @@
     .module('groceries')
     .controller('AssignController', AssignController);
 
-  AssignController.$inject = ['$scope', '$filter', '$uibModal', 'Notification', 'coreService', 'OrdersService'];
+  AssignController.$inject = ['$scope', '$state', '$filter', '$uibModal', 'Notification', 'coreService', 'OrdersService'];
 
-  function AssignController($scope, $filter, $uibModal, Notification, coreService, OrdersService) {
+  function AssignController($scope, $state, $filter, $uibModal, Notification, coreService, OrdersService) {
     var vm = this;
 
     vm.complete = false;
@@ -15,6 +15,7 @@
     vm.allowSave = true;
 
     vm.assign = assign;
+    vm.picklist = picklist;
 
     vm.orders = OrdersService.query({
       status: 'pending'
@@ -27,7 +28,7 @@
         Notification.info({
           message: 'Update successful!'
         });
-        // checkList();
+        checkList();
       }
 
       function errorCallback(res) {
@@ -35,19 +36,8 @@
       }
     }
 
-    // function checkList() {
-    //   vm.orders = vm.orders.every(function(order) {
-    //     return listItem.inCart || listItem.notAvailable;
-    //   });
-    //
-    //   if (vm.complete && vm.orders.length !== 0) {
-    //     checkout();
-    //   }
-    // }
-
     // Assign an order to a volunteer.
     function assign(order) {
-      console.log(order);
       vm.current = order;
 
       if (typeof vm.current.assignedTo === 'object') {
@@ -66,7 +56,7 @@
         vm.current.assignedTo = vm.assignedTo;
         vm.current.status = 'ordered';
 
-        vm.current.createOrUpdate()
+        updateOrder()
           .then(successCallback)
           .catch(errorCallback);
 
@@ -81,6 +71,53 @@
           vm.current.status = 'pending';
           vm.error = res.data.message;
         }
+      };
+      vm.modalCancel = function() {
+        modalInstance.dismiss('Cancel Clicked');
+      };
+
+      modalInstance.result.then(function() {
+        console.log('Open Checkout Interface');
+      }, function() {
+        console.info('modal-component dismissed at: ' + new Date());
+      });
+    }
+
+    function checkList() {
+      vm.complete = vm.orders.every(function(order) {
+        return order.status === 'ordered';
+      });
+
+      if (vm.complete && vm.orders.length !== 0) {
+        picklist();
+      }
+    }
+
+    function picklist() {
+      var header = 'Assignment Complete';
+      var message = 'All the orders have been assigned. Would you like to continue to the the picklist or stay here to review the orders?';
+      var buttonClass = 'success';
+
+      if (!vm.complete) {
+        header = 'Assignment Incomplete';
+        message = 'Some of the orders have not been assigned. Are you sure you would like to continue?';
+        buttonClass = 'danger';
+      }
+
+      var modalInstance = $uibModal.open({
+        animation: true,
+        template: '<div class="modal-header"><h3 class="modal-title">' + header + '</h3></div>' +
+          '<div class="modal-body">' + message + '</div>' +
+          '<div class="modal-footer">' +
+          '<button class="btn btn-default" type="button" ng-click="vm.modalCancel()">Stay Here</button>' +
+          '<button class="btn btn-' + buttonClass + '" type="button" ng-click="vm.modalOk()">Pick List</button>' +
+          '</div>',
+        scope: $scope
+      });
+
+      vm.modalOk = function() {
+        modalInstance.close('OK Clicked');
+        $state.go('groceries.list');
       };
       vm.modalCancel = function() {
         modalInstance.dismiss('Cancel Clicked');
